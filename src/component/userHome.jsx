1b27254e79c05cc24da5c7e43aed7fa6479b64df
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-04 11:55:11 -0600

    gigs now map onto user home

--- src/component/userHome.jsx
+++ src/component/userHome.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-
-export default class UserHome extends Component{
+import {connect} from 'react-redux'
+class UserHome extends Component{
   render(){
+
+    let gigMapped = this.props.gigs.map((gig) => {
+      return <div style={{border: "solid", width: "200px", borderRadius: "5px", padding: "5px"}} key={gig.id} urlMatch={gig.id}>
+      <h2>{gig.title}</h2>
+      
+      <p>Desc: {gig.description}</p>
+      <p>Time: {gig.total_time}</p>
+      <p>Rate: {gig.project_rate}</p>
+      <div>
+          <p>Billed: {gig.is_billed}</p>
+          <p>Paid: {gig.is_paid}</p>
+      </div>
+
+      
+</div>
+  })    
+
+
+
+
     return(
       <div>
-        User Home! 
+        User Home!
+
+{gigMapped}
+
       <Link to='/wizard'>
         <div style={{
           // height: '40px', 
@@ -25,3 +48,15 @@ export default class UserHome extends Component{
     )
   }
 }
+
+const mapStateToProps=(reduxState)=> {
+return {
+  gigs: reduxState.gigs
+}
+}
+
+const mapDispatchToProps= {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserHome)
