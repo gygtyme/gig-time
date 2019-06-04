60f418d6fc880ef1affd875a7fe2076e0e361ffc
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-04 11:36:47 -0600

    plus button added to userHome

--- src/component/userHome.jsx
+++ src/component/userHome.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router-dom'
 
 export default class UserHome extends Component{
   render(){
     return(
       <div>
         User Home! 
+      <Link to='/wizard'>
+        <div style={{
+          // height: '40px', 
+          width: '60px', 
+          border: '2px solid black', 
+          fontSize: '60px', 
+          position: 'absolute', 
+          bottom: '20px', 
+          right: "20px", 
+          borderRadius: '100%',
+          textAlign: 'center', 
+          padding:'20px'
+        
+        }}>+</div>
+        </Link>
       </div>
     )
   }
