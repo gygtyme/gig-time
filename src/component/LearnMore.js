ba016ca0041c9b18e40fdca0e82c7892c4baca01
sgwilliam <sdgwills@gmail.com>
2019-06-05 15:41:51 -0600

    updated learn more page adding video as well as react-iframe dependency

--- src/component/LearnMore.js
+++ src/component/LearnMore.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './styles/LearnMore.css';
+import Iframe from 'react-iframe';
 
 export default class LearnMore extends Component {
   constructor() {
@@ -8,10 +10,26 @@ export default class LearnMore extends Component {
 
   render() {
     return(
-      <div className='learnMoreBody'>
-        <div className='learnMoreMain'>
-          
+      <div>
+        <div className='learnMoreHero'>
+          <div className='heroText'>
+            <h2>Welcome to GIG Time!</h2>
+            <h4>We are fast, furious, and efficient!</h4>
+            <h2>Are You?</h2>
+          </div>
         </div>
+        <section className='learnMoreCardContainer'>
+          <div className='learnMoreCard'>You'll Get Hot Chicks</div>
+          <div className='learnMoreCard'>You'll Get The Fat Stacks</div>
+          <div className='learnMoreCard'>You'll Probably Get Free Drugs</div>
+        </section>
+        <section className='learnMoreVideo'>
+          <Iframe 
+            width="600" 
+            height="300" 
+            url="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          />
+        </section>
       </div>
     )
   }
