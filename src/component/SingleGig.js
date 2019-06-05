1cb4ad8e5851e0f8cdab861a31a25dcd90670908
tiago maffei <tiagomaffei@tiagos-MacBook-Pro.local>
2019-06-05 14:48:29 -0600

    total amount done

--- src/component/SingleGig.js
+++ src/component/SingleGig.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
-import {connect} from "react-redux"
+import { connect } from "react-redux"
 import Axios from 'axios';
-
 import Task from './Task'
+const ms = require('pretty-ms')
+
 
 class SingleGig extends Component {
     state = {
-        client: {}
+        client: {},
+        amountDue: 0
     }
-    componentDidMount(){
+    componentDidMount() {
         let gig = this.props.match.params.gig_id
         let id = gig.client_id
         this.getClient(id)
     }
 
     getClient = (id) => {
-        Axios.post("/api/clients", {id}).then(res => {
+        Axios.post("/api/clients", { id }).then(res => {
             this.setState({
                 client: res.data
             })
         })
     }
+
+    
     render() {
-        const {client} = this.state
+        const { client } = this.state
         let gig_id = this.props.match.params.gig_id
         let gig = this.props.gigs.filter(gig => +gig_id === +gig.id)
         gig = gig[0]
-        
+        console.log('look it here', gig)
         let gigDisplay = (this.props.firstName) ?
-        <div style={{border: "solid"}}>
-        <h2>{gig.title}</h2>
-        <p>Client name: {client.client_first} {client.client_last}</p>
-        <p>Client contact: {client.client_email} {client.client_phone}</p>
-        <p>Gig description: {gig.description}</p>
-        <p>Total development time: {gig.total_time}</p>
-        <p>Gig rate: {gig.project_rate}</p>
-        <p>{gig.is_paid}</p>
-        <p>{gig.is_billed}</p>
-        <div>
-            <Task gig={gig}/>
-        </div>
-        </div> : null
+            <div style={{ border: "solid" }}>
+                <h2>{gig.title}</h2>
+                <p>Client name: {client.client_first} {client.client_last}</p>
+                <p>Client contact: {client.client_email} {client.client_phone}</p>
+                <p>Gig description: {gig.description}</p>
+                <p>Total development time: {ms(gig.total_time)}</p>
+                <p>Gig rate: ${gig.project_rate.toFixed(2)}</p>
+                <p>Ammount due: ${((gig.total_time /1000/60/60)*gig.project_rate).toFixed(2)}</p>
+                <p>{gig.is_paid}</p>
+                <p>{gig.is_billed}</p>
+                
+                <div>
+                    <Task gig={gig} />
+                </div>
+            </div> : null
         return (
             <div>
                 {gigDisplay}
@@ -50,8 +56,8 @@ class SingleGig extends Component {
 }
 
 const mapStateToProps = (state) => {
-    let {gigs, firstName} = state
-    return {gigs, firstName}
+    let { gigs, firstName } = state
+    return { gigs, firstName }
 }
 
 export default connect(mapStateToProps)(SingleGig);
