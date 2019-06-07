6a7cc5a69dec358d202e3d7940b06aea8ee64ff2
sgwilliam <sdgwills@gmail.com>
2019-06-07 14:50:32 -0600

    res

--- server/Controllers/clientCtrl.js
+++ server/Controllers/clientCtrl.js
@@ -34,7 +34,6 @@ module.exports = {
 
     let { firstName, clientEmail } = req.body
 
-<<<<<<< HEAD
       var mailOptions = {
         from: 'update@gigtime.com',
         to: `${clientEmail}`,
@@ -88,63 +87,6 @@ module.exports = {
     to: `${email}`,
     subject: `A client has left you feedback on one of your gigs.`,
     text: `${firstName}, you have feedback on your gig ${gig.title}. The client's feedback is listed below. 
-=======
-    //needs to go to link localhost:3000/#/client-view/:gig_id
-
-    var nodemailer = require('nodemailer');
-
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'dropinappinfo@gmail.com',
-        pass: GOOGLE
-      }
-    });
-
-    var mailOptions = {
-      from: 'update@gigtime.com',
-      to: `${clientEmail}`,
-      subject: `A client has left you feedback on one of your gigs.`,
-      text: `<div>Hey ${firstName}! This is your update on the project: <a href='localhost:3000/#/client-view/${gig_id}'> localhost:3000/#/client-view/${gig_id} </a> click the link or paste it into your browser</div>
-        `
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-  },
-
-  sendFeedback: async (req, res) => {
-    console.log(req.session)
-    let dbInstance = req.app.get('db')
-
-
-    let { feedback, user_id, gig } = req.body
-
-    let user = await dbInstance.get_user_by_id(user_id)
-
-    user = user[0]
-
-    var nodemailer = require('nodemailer');
-
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'dropinappinfo@gmail.com',
-        pass: GOOGLE
-      }
-    });
-
-    var mailOptions = {
-      from: 'feedback@gigtime.com',
-      to: `${user.email}`,
-      subject: `A client has left you feedback on one of your gigs.`,
-      text: `${user.first_name}, you have feedback on your gig ${gig.title}. The client's feedback is listed below. 
->>>>>>> master
     ${feedback}
     `
     };
