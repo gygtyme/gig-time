d5fc2520fbb3556d8de2da115cda1afb4c94c0b2
sgwilliam <sdgwills@gmail.com>
2019-06-07 14:45:57 -0600

    resolved more conflicts

--- src/component/ClientView.js
+++ src/component/ClientView.js
@@ -15,23 +15,13 @@ class ClientView extends Component {
   }
   async componentDidMount() {
 let display= await axios.get(`/api/getSingleGig/${this.props.match.params.gig_id}`)
+  }
 
-<<<<<<< HEAD
   render() {
     let gigId= this.props.match.params.gig_id
     let gigToDisplay= this.props.gigs.find(el=>{
         return +el.id===+gigId
     })
-=======
-  this.setState({
-    displayGig: display.data
-  })
-
-
-}
-
-  render() {
->>>>>>> master
 
 let {displayGig}=this.state
 return(
