50e9abde340f3750c3cb287487891e724985983d
jbuchanan00 <josh.buchanan00@gmail.com>
2019-06-12 13:37:47 -0600

    navbar has been styled with hamburger on mobile

--- src/component/HamburgerMenu.js
+++ src/component/HamburgerMenu.js
@@ -26,25 +26,50 @@ class HamburgerMenu extends Component {
     this.props.push('/')
   }
   render() {
-    let links = this.props.firstName ? <div><Link className="sidebar-link" to="/userhome"><p onClick={this.sidebarOpen}>Gigs</p></Link>
-      <Link className="sidebar-link" to="/clientlist"><p onClick={this.sidebarOpen}>Clients</p></Link>
-      <Link to="/gighistory" className="sidebar-link"><p onClick={this.sidebarOpen}>Gig History</p></Link>
+    let sidebarLinks = this.props.firstName ? <div>
+      <div className="link-container">
+        <Link className="sidebar-link" to="/userhome"><p onClick={this.sidebarOpen}> Gigs</p></Link>
+      </div>
+      <div className="link-container">
+        <Link className="sidebar-link" to="/clientlist"><p onClick={this.sidebarOpen}> Clients</p></Link>
+      </div>
+      <div className="link-container">
+        <Link to="/gighistory" className="sidebar-link"><p onClick={this.sidebarOpen}> Gig History</p></Link>
+      </div>
+      <div className="logout-sidebar-container">
+        <button className="logout_button" onClick={this.logoutButton}>Logout</button>
+      </div>
+    </div> : null
+
+    let navbarLinks = this.props.firstName ? <div className="desktop-nav">
+      <div className="link-desktop-container">
+        <Link className="sidebar-link" to="/userhome"><p onClick={this.sidebarOpen}> Gigs</p></Link>
+      </div>
+      <div className="link-desktop-container">
+        <Link className="sidebar-link" to="/clientlist"><p onClick={this.sidebarOpen}> Clients</p></Link>
+      </div>
+      <div className="link-desktop-container-gig-history">
+        <Link to="/gighistory" className="sidebar-link"><p onClick={this.sidebarOpen}> Gig History</p></Link>
+      </div>
       <button className="logout_button" onClick={this.logoutButton}>Logout</button>
     </div> : null
     return (
-      <div className="nav_container">
-        <Sidebar rootClassName="mobileNavView"
-          sidebar={<div><p>Sidebar Content</p>
-            <div className="h-menu-link">{links}</div></div>}
-          open={this.state.mobileBarOpen}
-          onSetOpen={this.sidebarOpen}
-          styles={{ sidebar: { background: "#F9F8F8", position: "fixed" } }}
-          pullRight={true}
-        >
-          <div className="hamburger-button-container">
-            <button onClick={this.sidebarOpen} className="hamburger-icon-button"><i className="fas fa-bars fa-5x"></i></button>
-          </div>
-        </Sidebar>
+      <div className="total-navbar-container">
+        <div className="nav_container">
+          <Sidebar rootClassName="mobileNavView"
+            sidebar={<div><div className="sidebar-header"><div className="menu-header-sidebar">Menu</div></div>
+              <div className="h-menu-link">{sidebarLinks}</div></div>}
+            open={this.state.mobileBarOpen}
+            onSetOpen={this.sidebarOpen}
+            styles={{ sidebar: { background: "#969A97", position: "fixed" } }}
+            pullRight={true}
+          >
+            <div className="hamburger-button-container">
+              <button onClick={this.sidebarOpen} className="hamburger-icon-button"><i className="fas fa-bars fa-5x"></i></button>
+            </div>
+          </Sidebar>
+        </div>
+        {navbarLinks}
       </div>
     )
   }
