4698def01f92aef4159f42c4f9a2aabe03573b90
sgwilliam <sdgwills@gmail.com>
2019-06-12 15:01:54 -0600

    finished all tests

--- src/component/ClientView.js
+++ src/component/ClientView.js
@@ -28,13 +28,13 @@ class ClientView extends Component {
 
   timeConversion = (millisec) => {
 
-    var seconds = (millisec / 1000).toFixed(1);
+    var seconds = (millisec / 1000).toFixed(2);
 
-    var minutes = (millisec / (1000 * 60)).toFixed(1);
+    var minutes = (millisec / (1000 * 60)).toFixed(2);
 
-    var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+    var hours = (millisec / (1000 * 60 * 60)).toFixed(2);
 
-    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(2);
 
     if (seconds < 60) {
         return seconds + " Sec";
@@ -45,6 +45,12 @@ class ClientView extends Component {
     } else {
         return days + " Days"
     }
+  }
+
+  moneyConversion = (rate, time) => {
+    let msToTime = time / 1000 / 60 / 60
+    let totalMoney = msToTime * rate
+    return totalMoney
   }
 
   render() {
